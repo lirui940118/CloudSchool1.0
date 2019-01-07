da0bf1fc00f95c412f630a4b7f9bfe4f5a7f139a
lirui <44960229+lirui940118@users.noreply.github.com>
2019-01-07 15:50:35 +0800

    创建项目

---
+++ CloudSchool/src/main/java/com/CloudSchool/CloudSchoolApplication.java
@@ -0,0 +1,14 @@
+package com.CloudSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSchoolApplication.class, args);
+	}
+
+}
+
