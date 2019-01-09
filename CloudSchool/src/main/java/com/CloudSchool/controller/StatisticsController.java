735d938e6c8b012f06996587332e41dcf5affd27
lirui <44960229+lirui940118@users.noreply.github.com>
2019-01-09 13:34:19 +0800

    重新改了gk的表

--- CloudSchool/src/main/java/com/CloudSchool/controller/StatisticsController.java
+++ CloudSchool/src/main/java/com/CloudSchool/controller/StatisticsController.java
@@ -2,7 +2,6 @@ package com.CloudSchool.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("statistics")
