af299f0acbfffda92ca3a0830a6998a9e110b065
ajax12138 <1741303700@qq.com>
2019-01-11 20:16:01 +0800

    1

--- CloudSchool/src/main/java/com/CloudSchool/controller/GkController.java
+++ CloudSchool/src/main/java/com/CloudSchool/controller/GkController.java
@@ -5,15 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/uuu")
+@RequestMapping("/GK")
 public class GkController {
-	@RequestMapping("/kkk")
-	@ResponseBody
-	public String kkk() {
-		return "gk";
-	}
-	@RequestMapping("/iii")
-	public String iii() {
-		return "index";
-	}
+	
 }
