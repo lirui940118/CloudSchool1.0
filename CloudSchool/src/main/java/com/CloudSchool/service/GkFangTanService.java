7ffc8785451c4d2792ec94f7f82c1ec498cb8c1b
ajax12138 <1741303700@qq.com>
2019-01-14 11:37:54 +0800

    55

--- CloudSchool/src/main/java/com/CloudSchool/service/GkFangTanService.java
+++ CloudSchool/src/main/java/com/CloudSchool/service/GkFangTanService.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.CloudSchool.domain.GkFangTan;
 import com.CloudSchool.domain.GkInterview;
 import com.CloudSchool.domain.GkPageBean;
 
 public interface GkFangTanService {
 	//查询所有访谈
-	GkPageBean<GkInterview> queryAllFangTan(@Param("currentPage")Integer currentPage,@Param("pageSize")Integer pageSize);
+	GkPageBean<GkInterview> queryAllFangTan(GkFangTan ft);
 	
 }
--- CloudSchool/src/main/java/com/CloudSchool/service/impl/GkFangTanServiceImpl.java
+++ CloudSchool/src/main/java/com/CloudSchool/service/impl/GkFangTanServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.CloudSchool.dao.GkInterviewMapper;
+import com.CloudSchool.domain.GkFangTan;
 import com.CloudSchool.domain.GkInterview;
 import com.CloudSchool.domain.GkPageBean;
 import com.CloudSchool.service.GkFangTanService;
@@ -16,11 +17,18 @@ public class GkFangTanServiceImpl implements GkFangTanService{
 	@Autowired
 	GkInterviewMapper gkInterviewMapper;
 	//查询所有访谈
-	public GkPageBean<GkInterview> queryAllFangTan(Integer currentPage, Integer pageSize) {
-		int index = (currentPage-1)*pageSize; //下标
-		List<GkInterview> list = gkInterviewMapper.queryAllFangTan(index, pageSize);
-		int totalSize = (int)gkInterviewMapper.queryAllFangTanCount();
-		GkPageBean<GkInterview> page = new GkPageBean<GkInterview>(currentPage, pageSize, totalSize, list);
+	public GkPageBean<GkInterview> queryAllFangTan(GkFangTan ft) {
+		//int index = (ft.getCurrentPage()-1)*ft.getPageSize(); 
+		GkFangTan ft2 = new GkFangTan();
+		ft2.setCurrentPage((ft.getCurrentPage()-1)*ft.getPageSize());//下标
+		ft2.setEndtime(ft.getEndtime());
+		ft2.setObjectname(ft.getObjectname());
+		ft2.setPageSize(ft.getPageSize());
+		ft2.setPeoplename(ft.getPeoplename());
+		ft2.setStarttime(ft.getStarttime());
+		List<GkInterview> list = gkInterviewMapper.queryAllFangTan(ft2);
+		int totalSize = (int)gkInterviewMapper.queryAllFangTanCount(ft2);
+		GkPageBean<GkInterview> page = new GkPageBean<GkInterview>(ft.getCurrentPage(), ft.getPageSize(), totalSize, list);
 		return page;
 	}
 }
