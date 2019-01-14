b68707a018d86b2534b10a8ee34d2002f4cef762
lirui <44960229+lirui940118@users.noreply.github.com>
2019-01-14 14:20:47 +0800

    更新了commission

---
+++ CloudSchool/src/main/java/com/CloudSchool/domain/Commission.java
@@ -0,0 +1,103 @@
+package com.CloudSchool.domain;
+
+public class Commission {
+    private Integer id;
+
+    private Integer tid;
+
+    private String describe;
+
+    private String accessory;
+
+    private Integer status;
+
+    private Integer cid;
+
+    private String user1;
+
+    private String user2;
+
+    private String user3;
+
+    private String user4;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTid() {
+        return tid;
+    }
+
+    public void setTid(Integer tid) {
+        this.tid = tid;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getAccessory() {
+        return accessory;
+    }
+
+    public void setAccessory(String accessory) {
+        this.accessory = accessory;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public String getUser1() {
+        return user1;
+    }
+
+    public void setUser1(String user1) {
+        this.user1 = user1;
+    }
+
+    public String getUser2() {
+        return user2;
+    }
+
+    public void setUser2(String user2) {
+        this.user2 = user2;
+    }
+
+    public String getUser3() {
+        return user3;
+    }
+
+    public void setUser3(String user3) {
+        this.user3 = user3;
+    }
+
+    public String getUser4() {
+        return user4;
+    }
+
+    public void setUser4(String user4) {
+        this.user4 = user4;
+    }
+}
