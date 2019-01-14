19f0761defc88d511336f102570833547326224d
tianzhixiang <1040522179@qq.com>
2019-01-14 17:51:44 +0800

    1.14

--- CloudSchool/src/main/java/com/CloudSchool/controller/CreateClazzController.java
+++ CloudSchool/src/main/java/com/CloudSchool/controller/CreateClazzController.java
@@ -13,54 +13,65 @@ import com.CloudSchool.domain.ClazzInfo;
 import com.CloudSchool.domain.Clazzcourseteacher;
 import com.CloudSchool.domain.Clazzstudent;
 import com.CloudSchool.service.ClazzService;
+import com.alibaba.fastjson.JSON;
 
 @Controller
 public class CreateClazzController {
 	@Autowired
 	ClazzService cs;
-	@RequestMapping(value="createClazz",method=RequestMethod.GET)
+	@RequestMapping(value="/createClazz",method=RequestMethod.GET)
 	public String toCreateClazz() {
 		System.out.println("跳转页面-开班");
 		return "tzx/createClazz";
 	}
+	/**
+	 * 获取开班可选信息
+	 */
 	@ResponseBody
-	@RequestMapping(value="craeteClazz",method=RequestMethod.POST)
+	@RequestMapping("/getOptionOfCreateClazz")
+	public String getOptionOfCreateClazz() {
+		System.out.println("查询-------获取开班可选信息：版本、年级、专业、老师（按课程）、学生");
+		return "获取开班可选信息";
+	}
+	@ResponseBody
+	@RequestMapping(value="/craeteClazz",method=RequestMethod.POST)
 	public int createClazz(ClazzInfo c) {
 //		c.setId(11);
 		System.out.println("开班-新增班级");
-
+		System.out.println(JSON.toJSONString(c));
 		//预添加数据------------
-		c.setCname("AT1717");
-		c.setGid(1);
-		c.setMid(1);
-		c.setCount(30);
-		c.setStatus(0);
-		c.setPc("20190001");
-		c.setSlist(new ArrayList<Clazzstudent>());
-		c.setTlist(new ArrayList<Clazzcourseteacher>());
-		Clazzstudent cls=new Clazzstudent();
-		Clazzstudent cls1=new Clazzstudent();
-		cls.setSid(55);
-		cls1.setSid(66);
-		c.getSlist().add(cls);
-		c.getSlist().add(cls1);
-		Clazzcourseteacher cct=new Clazzcourseteacher();
-		Clazzcourseteacher cct1=new Clazzcourseteacher();
-		cct.setCourseid(11);
-		cct.setTid(22);
-		cct.setIsmaster(1);
-		cct.setIsteacher(0);
-		cct1.setCourseid(12);
-		cct1.setTid(23);
-		cct1.setIsmaster(0);
-		cct1.setIsteacher(1);
-		c.getTlist().add(cct);
-		c.getTlist().add(cct1);
+//		c.setCname("AT1717");
+//		c.setGid(1);
+//		c.setMid(1);
+//		c.setCount(30);
+//		c.setStatus(0);
+//		c.setPc("20190001");
+//		c.setSlist(new ArrayList<Clazzstudent>());
+//		c.setTlist(new ArrayList<Clazzcourseteacher>());
+//		Clazzstudent cls=new Clazzstudent();
+//		Clazzstudent cls1=new Clazzstudent();
+//		cls.setSid(55);
+//		cls1.setSid(66);
+//		c.getSlist().add(cls);
+//		c.getSlist().add(cls1);
+//		Clazzcourseteacher cct=new Clazzcourseteacher();
+//		Clazzcourseteacher cct1=new Clazzcourseteacher();
+//		cct.setCourseid(11);
+//		cct.setTid(22);
+//		cct.setIsmaster(1);
+//		cct.setIsteacher(0);
+//		cct1.setCourseid(12);
+//		cct1.setTid(23);
+//		cct1.setIsmaster(0);
+//		cct1.setIsteacher(1);
+//		c.getTlist().add(cct);
+//		c.getTlist().add(cct1);
 		//预添加数据------------
 		return cs.CreateClass(c);
+//		return JSON.toJSONString(c);
 	}
 	@ResponseBody
-	@RequestMapping("createClazz1")
+	@RequestMapping("/createClazz1")
 	public void toCreateClazz1() {
 		System.out.println("111111111-------直接调开班方法");
 		createClazz(new ClazzInfo());
--- CloudSchool/src/main/java/com/CloudSchool/controller/NoticeController.java
+++ CloudSchool/src/main/java/com/CloudSchool/controller/NoticeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.CloudSchool.domain.CqjUser;
 import com.CloudSchool.domain.Notice;
 import com.CloudSchool.service.NoticeService;
 import com.CloudSchool.service.imgcompressService;
@@ -28,9 +29,11 @@ public class NoticeController {
 	}
 	@ResponseBody
 	@RequestMapping(value="/addnotice" ,method=RequestMethod.POST)
-	public String addNotice() {
-		System.out.println("新增-添加通知");
-		return "添加通知成功！";
+	public int addNotice(HttpSession session,Notice n) {
+		CqjUser u=(CqjUser)session.getAttribute("user");
+		System.out.println("新增-添加通知"+u.getUserid());
+		n.setSender(u.getUserid());
+		return ns.insert(n);
 	}
 	@RequestMapping("/notice")
 	public String toNotice() {
@@ -39,9 +42,9 @@ public class NoticeController {
 	}
 	@ResponseBody
 	@RequestMapping("/noticeList")
-	public List<Notice> NoticeList() {
+	public List<Notice> NoticeList(String time,String str) {
 		System.out.println("查询-通知列表");
-		return ns.query();
+		return ns.query(time,str);
 	}
 	@RequestMapping("/noticeDetail")
 	public String toNoticeDetail(HttpSession session,int nid) {
@@ -55,6 +58,19 @@ public class NoticeController {
 		System.out.println("查询-通知详情");
 		int nid=(int) session.getAttribute("noticeid");
 		return ns.selectByPrimaryKey(nid);
+	}
+	/**
+	 * 查询个人id、名字信息
+	 * @param session
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/querpersonerInfo" )
+	public CqjUser uinfo(HttpSession session) {//名字存在standby1中
+		CqjUser u=(CqjUser)session.getAttribute("user");
+		CqjUser u1=ns.querpersonerInfo(u.getUserid());
+		System.out.println("查询个人信息："+u.getUserid());
+		return u1;
 	}
 	
 	
