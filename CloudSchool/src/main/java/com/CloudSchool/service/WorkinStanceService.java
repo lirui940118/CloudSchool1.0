29c90809e0a1a6d58f16056c34c0373b8d863659
zjf0109 <1838228605@qq.com>
2019-01-15 08:10:42 +0800

    作业实例

---
+++ CloudSchool/src/main/java/com/CloudSchool/service/WorkinStanceService.java
@@ -0,0 +1,8 @@
+package com.CloudSchool.service;
+
+import com.CloudSchool.domain.zjfvo.WorkPublishParam;
+
+public interface WorkinStanceService {
+	/*作业发布*/
+	public int publishWork(WorkPublishParam obj);
+}
