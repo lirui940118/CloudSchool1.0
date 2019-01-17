14b3ee9d543a6e72d9a4f5e8d7479b3092730cfb
tianzhixiang <1040522179@qq.com>
2019-01-17 11:17:18 +0800

    1.17 合并前改config

--- CloudSchool/src/main/java/com/CloudSchool/config/InterceptorConfig.java
+++ CloudSchool/src/main/java/com/CloudSchool/config/InterceptorConfig.java
@@ -1,5 +1,6 @@
 package com.CloudSchool.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -7,25 +8,27 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.CloudSchool.Interceptor.LoginInterceptor;
+
 @Configuration
 public class InterceptorConfig extends WebMvcConfigurationSupport {
 	
-	@Override
-	protected void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowCredentials(true);
-		// TODO Auto-generated method stub
-		super.addCorsMappings(registry);
-	}
-	
-	
+	@Autowired
+	LoginInterceptor loginInterceptor;
+
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**","/baidueditor/**").addResourceLocations("classpath:/static/","file:e:\\img\\");
 		super.addResourceHandlers(registry);
 	}
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/static/**","/user/login","/user/goLogin","/baidueditor/**");
+		registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/static/**",
+				"/user/login", "/user/goLogin","/user/goIndex","/user/goHome","/user/getInfo","/error","/module/getInfoModule");
+	}
+	@Override
+	protected void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowCredentials(true);
+		super.addCorsMappings(registry);
 	}
 }
